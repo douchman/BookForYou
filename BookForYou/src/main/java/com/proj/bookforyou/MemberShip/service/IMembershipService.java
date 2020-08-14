dbcab6c2d8897d65ec85de4afc1b7f4b530b9703
Eunji.Baek <qordmswl1541@naver.com>
2020-08-14 09:30:52 +0900

    Update IMembershipService.java

--- BookForYou/src/main/java/com/proj/bookforyou/MemberShip/service/IMembershipService.java
+++ BookForYou/src/main/java/com/proj/bookforyou/MemberShip/service/IMembershipService.java
@@ -1,5 +1,10 @@
 package com.proj.bookforyou.MemberShip.service;
 
+import com.proj.bookforyou.MemberShip.BfuMember;
+
 public interface IMembershipService {
 
+	public String isExistId(BfuMember member);
+	public String isExistNickName(BfuMember member);
+	public String memberProc(BfuMember member);
 }
